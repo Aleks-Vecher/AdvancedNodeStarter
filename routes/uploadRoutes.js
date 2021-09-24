e62e5e69419c05a273da9fefba5c122551c648fa
Aleks-Vecher <aleks.vecher@gmail.com>
2021-09-24 16:58:12 +0300

    aws s3

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -3,10 +3,15 @@ const keys = require('../config/keys')
 const requireLogin = require('../middlewares/requireLogin')
 const uuid = require('uuid/v1')
 
-const s3 = new AWS.S3({
-  accessKeyId: keys.accessKeyId,
-  secretAccessKey: keys.secretAccessKey,
-})
+const s3 = new AWS.S3(
+    {
+      credentials: {
+        accessKeyId: keys.accessKeyId,
+        secretAccessKey: keys.secretAccessKey,
+      },
+      region: 'eu-central-1',
+    }
+    )
 
 module.exports = app => {
   app.get('/api/upload', requireLogin, (req, res) => {
